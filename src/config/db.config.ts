ec7f1f0965a0a7d6e204d48eca3fdc7896cbcaa6
Nandhinivelu <139103042+nandhini-converse@users.noreply.github.com>
2023-11-15 13:30:20 +0530

    env file added

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,15 +2,17 @@ import { Sequelize } from "sequelize-typescript";
 import { Userinfo } from "../models/userinfo.model";
 import { Role } from "../models/role.model";
 import { Package } from "../models/package.model";
+require('dotenv').config();
 
 const connection = new Sequelize({
   dialect: "mysql",
-  host: "localhost",
-  username: "root",
-  password: "Converse@123",
-  database:"travelinwebsite",
+  host: process.env.DB_HOST||'localhost',
+  username: process.env.DB_USERNAME||'root',
+  password: process.env.DB_PASSWORD||'Converse@123',
+  database: process.env.DB_NAME||'travelinwebsite',
   logging: false,
-  models: [Userinfo, Role,Package],
+  models: [Userinfo, Role, Package],
 });
+
 
 export default connection;
